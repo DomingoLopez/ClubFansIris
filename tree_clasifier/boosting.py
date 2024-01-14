47c6580b7c0bb7987f941ddf97384491d2eb8dd9
Alejandro Manzanares <alexmnzlms@gmail.com>
2024-01-14 17:50:49 +0100

    Retocado boosting

--- tree_clasifier/boosting.py
+++ tree_clasifier/boosting.py
@@ -7,7 +7,7 @@ import time
 class AdaBoost:
     def __init__(self):
         self.alphas = []
-        self.G_M = []
+        self.fitted_clf = []
         self.depth = 0
         self.n_trees = None
         self.training_errors = []
@@ -24,38 +24,36 @@ class AdaBoost:
         return w_i * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
 
     def fit(self, X, y, depth=4, n_trees = 100):
-        # Clear before calling
         self.alphas = [] 
         self.training_errors = []
         self.depth = depth
         self.n_trees = n_trees
 
-        # Iterate over M weak classifiers
         for m in range(n_trees):
             
             # Set weights for current boosting iteration
             if m == 0:
                 w_i = np.ones(len(y)) * 1 / len(y)  # At m = 0, weights are all the same and equal to 1 / N
             else:
-                # (d) Update w_i
+                # Update w_i
                 w_i = self.update_weights(w_i, alpha_m, y, y_pred)
             
-            # (a) Fit weak classifier and predict labels
-            G_m = tree.DecisionTreeClassifier(max_depth = self.depth)
-            G_m.fit(X, y, sample_weight = w_i)
-            y_pred = G_m.predict(X)
+            # Fit weak classifier and predict labels
+            clf = tree.DecisionTreeClassifier(max_depth = self.depth)
+            clf.fit(X, y, sample_weight = w_i)
+            y_pred = clf.predict(X)
             
-            self.G_M.append(G_m) # Save to list of weak classifiers
+            self.fitted_clf.append(clf) # Save to list of weak classifiers
 
-            # (b) Compute error
+            # Compute error
             error_m = self.compute_error(y, y_pred, w_i)
             self.training_errors.append(error_m)
 
-            # (c) Compute alpha
+            # Compute alpha
             alpha_m = self.compute_alpha(error_m)
             self.alphas.append(alpha_m)
 
-        assert len(self.G_M) == len(self.alphas)
+        assert len(self.fitted_clf) == len(self.alphas)
 
     def predict(self, X):
         weak_preds = pd.DataFrame(index = range(len(X)), columns = range(self.n_trees)) 
@@ -64,7 +62,7 @@ class AdaBoost:
         pred_list = []
         alpha_list = []
         for m in range(self.n_trees):
-            pred_list.append(self.G_M[m].predict(X))
+            pred_list.append(self.fitted_clf[m].predict(X))
             alpha_list.append(self.alphas[m])
 
         alpha_list = np.array(alpha_list)
